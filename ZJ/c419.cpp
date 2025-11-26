53dc57d7fdfa9b0b740577b1c78ed31634fa7931
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 15:36:41 +0800

    Create c419.cpp

---
+++ ZJ/c419.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    cin.sync_with_stdio(0) ;
+    cin.tie(0) ;
+
+    int n ;
+    cin>>n ;
+    for(int i = 1 ; i<=n ; i++){
+        for(int j = n-i ; j>0 ; j--){
+            cout<<"_" ; 
+        }
+        for(int k = 1 ; k<=i ; k++){
+            cout<<"*" ;
+        }
+        cout<<"\n" ;
+    }
+
+  return 0 ;
+}
