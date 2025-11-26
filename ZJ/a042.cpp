454badc5a444d45cb7f539291ae37bd21674c5aa
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:34:05 +0800

    Create a042.cpp

---
+++ ZJ/a042.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ;
+#define long long int  
+
+
+int main(){
+      cin.sync_with_stdio(0) ;
+      cin.tie(0) ;
+
+     int x;  
+         while (cin >> x)
+        {
+         cout << x * (x - 1) + 2 <<"\n";
+        }
+    
+     return 0 ;
+}
