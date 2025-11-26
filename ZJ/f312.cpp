1bfcb1369657d1b89f9c9ed4b308294ffd1f4ca2
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:36:49 +0800

    Update f312.cpp

--- ZJ/f312.cpp
+++ ZJ/f312.cpp
@@ -1,14 +1,14 @@
-#include<bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int A1,A2,A3,B1,B2,B3,n,Y1,Y2;
-    cin >>A1>>A2>>A3>>B1>>B2>>B3>>n;
-    int max = 0 ;
-    for(int i=0;i<=n;i++){
-        Y1=A1*i*i+A2*i+A3;
-        Y2=B1*(n-i)*(n-i)+B2*(n-i)+B3; 
-        max=std::max(Y1+Y2,max);
+int a1,b1,c1,a2,b2,c2,n;
+int main(){
+    cin>>a1>>b1>>c1>>a2>>b2>>c2>>n;
+    int ans=INT_MIN;
+    for(int x1=0;x1<=n;x1++){
+    	int x2=n-x1;
+    	ans=max(ans,a1*x1*x1+b1*x1+c1+a2*x2*x2+b2*x2+c2);
     }
-    cout<<max;
-    return 0;
+    cout<<ans;
+
+    return 0 ;
 }
