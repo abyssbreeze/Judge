9928fccaf8755c272e311050e2d69ad97443762d
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:48:27 +0800

    Create b968.cpp

---
+++ ZJ/b968.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0) ;
+	cin.tie(0) ;
+
+	string boy,girl ;
+	getline(cin,girl) ;
+	getline(cin,boy) ;
+	cout<<girl<<" and "<<boy<<" sitting in the tree\n" ;
+}
