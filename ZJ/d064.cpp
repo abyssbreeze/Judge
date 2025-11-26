79bd4d2f7418a0301fd69c4b24c0f6a56387b950
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:46:47 +0800

    Create d064.cpp

---
+++ ZJ/d064.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0) ;
+	cin.tie(0) ;
+
+	int i ;
+	cin>>i ;
+	if(i%2==0){
+		cout<<"Even\n" ;
+	}
+	else{
+		cout<<"Odd\n" ;
+	}
+}
