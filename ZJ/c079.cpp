538bb537a7b2457815b2a7e9111a6b40c587ee72
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:55:46 +0800

    Create c079.cpp

---
+++ ZJ/c079.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k,sum;
+    while(cin>>n>>k){
+        sum=n;
+        while(n>=k){
+            sum+=(n/k);
+            n=(n/k)+(n%k);
+        }
+        cout<<sum<<endl;
+    }
+}
