f3e8474a1de8aef45bc849a93f9e7b9eba151a98
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:51:19 +0800

    Create d881.cpp

---
+++ ZJ/d881.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+      cin.sync_with_stdio(0) ;
+      cin.tie(0) ;
+
+      int d ;
+      int S ;
+      while(cin>>d){
+        S = 1275 + (50*49*48)/(1*2*3)*d ;
+      cout<<S<<"\n" ;
+      }
+      
+      return 0 ;
+}
