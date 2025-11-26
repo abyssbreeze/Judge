9f3515da5abce48abd535cd4eefac4ab87062f25
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 15:14:00 +0800

    Create e357.cpp

---
+++ ZJ/e357.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int f(int x){
+	if(x==1){
+		return 1 ;
+	}
+	else if(x%2==0){
+		return f(x/2) ;
+	}
+	else{
+	return f(x-1) + f(x+1) ;	
+	}
+}
+
+int main(){
+	int x ;
+	cin>>x ;
+	cout<<f(x)<<"\n" ;
+}
