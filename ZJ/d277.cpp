70b807c0163499d4cfa604e37eae14079cee1903
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 19:38:09 +0800

    Create d277.cpp

---
+++ ZJ/d277.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+
+  int a;
+
+  while(cin>>a){
+
+    if(a%2==0||a==0){
+
+      cout<<a<<endl;
+
+    }
+
+    else cout<<a-1<<endl;
+
+  }
+
+  return 0 ;
+
+}
