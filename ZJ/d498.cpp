d037290b1eb60c7a0448d7393bf68a915b7eaf66
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:30:39 +0800

    Create d498.cpp

---
+++ ZJ/d498.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n ;
+	int i= 1 ;
+	cin>>n ;
+	while(i<=n){
+		cout<<"I don't say swear words!\n" ;
+		i++ ;
+	}
+	return 0 ;
+	
+}
