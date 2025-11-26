30749f36b4e5178ecce439407a3c1d582d24d3fb
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 19:34:33 +0800

    Create a861.cpp

---
+++ ZJ/a861.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std ;
+
+#define ll long long 
+
+
+
+int main(){
+    cin.sync_with_stdio(0) ; 
+    cin.tie(0) ;
+
+    int h , w ;
+    int p ;
+    while(cin>>h>>w){
+        p = (h+w)*2 ;
+        cout<<p<<'\n' ;
+    }
+
+
+    return 0 ; 
+
+}
