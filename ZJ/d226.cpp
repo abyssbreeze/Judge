71c327d7148a2def0d56eb7fa4aeb543dc34d41c
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:36:05 +0800

    Create d226.cpp

---
+++ ZJ/d226.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std ;
+
+
+int main(){
+	ios::sync_with_stdio(0) ;
+	cin.tie(0) ;
+    int v ;
+    int t ;
+    int ans ;
+    while(cin>>v>>t){
+    	ans = v*2*t ;
+    	cout<<ans<<endl ;
+	}
+}
