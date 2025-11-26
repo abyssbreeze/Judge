4809ce6012b3bcb863d3832ab3615bb19b6f2220
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:57:00 +0800

    Update d065.cpp

--- ZJ/d065.cpp
+++ ZJ/d065.cpp
@@ -1,16 +1,8 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    int a,b,c;
-    cin >> a >> b >> c;
-    if (a >= b && a >= c)
-        cout << a << endl;
-    else if (b >= a && b >= c)
-        cout << b << endl;
-    else
-        cout << c << endl;
-    return 0;
+#include<bits/stdc++.h>
+using namespace std ;
+	
+int main(){
+	int a,b,c ;
+	cin>>a>>b>>c ;
+	cout<<max({a,b,c})<<"\n" ;
 }
