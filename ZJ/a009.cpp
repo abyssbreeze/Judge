463cabe1272045f550e7d53585034cb1bce95451
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:52:45 +0800

    Create a009.cpp

---
+++ ZJ/a009.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+	string str ;
+	string ans ;
+	int k ;
+	while( getline(cin,str) ){
+		for(int i = 0 ; i<str.size() ; i++ ){
+			str[i] = str[i]-7 ;
+			cout<<str[i] ;
+		}
+	}
+	
+	
+}
