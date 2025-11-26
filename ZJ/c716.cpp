26de9f500861825cd005c0cb888d83d3b03f5c52
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:32:51 +0800

    Create c716.cpp

---
+++ ZJ/c716.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+#define long long int  
+
+    
+int main(){
+      cin.sync_with_stdio(0) ;
+      cin.tie(0) ;
+
+     string str ;
+     getline(cin,str) ;
+
+     cout<<"Go, "<<str<<", go go" ;
+    
+     return 0 ;
+}
