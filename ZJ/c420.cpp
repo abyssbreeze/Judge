3381c22ed22bc813ef3d407444b781cdab509a90
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:59:09 +0800

    Create c420.cpp

---
+++ ZJ/c420.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  int n ;
+cin>>n  ;
+for(int i = 1 ; i <= n ; i++){
+  for(int j = n-i ; j>0 ; j--){ 
+    cout<<"_" ;
+  }
+  for (int k = 1 ; k <=2*i-1  ; k++){
+    cout<<"*" ;
+  }
+  for(int j = n-i ; j>0 ; j--){ 
+    cout<<"_" ;
+  }
+  cout<<"\n" ;
+}
+  return 0 ;
+}
