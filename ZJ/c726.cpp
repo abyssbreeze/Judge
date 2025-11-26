f42303e95bdf90d3f00b162bfe2bc36d97312bf9
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:52:14 +0800

    Create c726.cpp

---
+++ ZJ/c726.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+      cin.sync_with_stdio(0) ;
+      cin.tie(0) ;
+
+      string gn,bn ;
+      cin>>gn>>bn ;
+      cout<<gn<<" and "<<bn<< " sitting in the tree\n" ;
+
+      return 0 ;
+}
