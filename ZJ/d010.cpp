eb2f627522701df8c9e5cba4bb5486262ddfce2c
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:32:59 +0800

    Create d010.cpp

---
+++ ZJ/d010.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std ;
+
+
+int main(){
+    int n ;
+    while(cin>>n){
+		int s = 0 ;
+    	for( int i = 1 ; i<n ; i++){
+    		if(n%i == 0){
+    			s = s+i ;
+			}
+			else if(n%i!=0){
+				s = s ;
+			}
+		}
+		if(s>n){
+			cout<<"盈數 \n" ;
+		}
+		else if(s==n){
+			cout<<"完全數 \n" ;
+		}
+		else if(s<n){
+			cout<<"虧數 \n" ;
+		} 
+	}
+}
