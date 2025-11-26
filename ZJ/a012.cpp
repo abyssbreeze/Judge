e95862b4c8fb0f5360ae03fb741286bcdfbb6b72
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:39:58 +0800

    Create a012.cpp

---
+++ ZJ/a012.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+    using namespace std ;
+
+
+    int main(){
+      cin.sync_with_stdio(0) ;
+      cin.tie(0) ;
+
+      long long m,n ;
+      cin>>m>>n ;
+      cout<<abs(m-n)<<"\n" ;
+      
+      return 0 ;
+}
