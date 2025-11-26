e0ebb8d3af73ab32c94b9da3678570945a08dbc7
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 12:51:46 +0800

    Create c185.cpp

---
+++ ZJ/c185.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main(){
+	ios::sync_with_stdio(0) ;
+	cin.tie(0) ;
+
+	string  h  = "Hey " ;
+	string name ;
+	getline(cin,name) ;
+	cout<<h<<name<<"\n" ;
+	
+}
