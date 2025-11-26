bf074f54e26a947f7e7fdc6db769a46db003b72f
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:58:32 +0800

    Create i399.cpp

---
+++ ZJ/i399.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, sum[10] = {0}, Max = 0;
+  cin >> a >> b >> c;
+  sum[a]++; sum[b]++; sum[c]++; 
+  
+  for(int i=1; i<=9; i++) Max = max(Max, sum[i]); 
+  cout << Max << ' ';
+
+  for(int i=9; i>=1; i--){
+    if(sum[i] > 0) cout << i << ' ';
+  }
+  
+  return 0;
+}
