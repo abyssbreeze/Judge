1ac8d13ba044732f848a872c49c89314ffb3f8c7
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2025-11-26 18:38:49 +0800

    Create f700.cpp

---
+++ ZJ/f700.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+int main(){
+      cin.sync_with_stdio(0) ;
+      cin.tie(0) ;
+
+      string str ;
+      int len ;
+      getline(cin,str) ;
+      len = str.size() ;
+      cout<<str<<"\n" ;
+      for (int i = 0; i < len; i++)
+      {
+        cout<<"~" ;
+      }
+
+  return 0 ;
+}
