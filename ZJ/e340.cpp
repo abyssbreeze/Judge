d7bf8c9d82fe91031c033386ca48e609a464c086
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2026-03-19 16:59:34 +0800

    Update e340.cpp

--- ZJ/e340.cpp
+++ ZJ/e340.cpp
@@ -3,33 +3,27 @@
 using namespace std;
 
 
-int main()
-{
+int main(){
     int n ;
     cin>>n ;
     long long arr[n] ;
     long long sum[n] ;
     long long diff[n] ;
-    for (int i = 0 ; i < n ; i++)
-    {
+    for (int i = 0 ; i < n ; i++){
         cin>>arr[i] ;
     }
     sum[0] = arr[0] ;
     diff[0] = sum[0] ;
-    for (int i =  1 ; i < n; i++)
-    {
+    for (int i =  1 ; i < n; i++){
         sum[i] = sum[i-1] + arr[i] ;
     }
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++){
         diff[i] = arr[i] - arr[i-1] ;
     }
     
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout<<diff[i]<<" " ;
     }
-
 
     return 0 ;
 
