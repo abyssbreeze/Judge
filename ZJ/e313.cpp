37d2891199427ec2cd414185b1d19af4cebf4e18
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2026-03-26 23:40:40 +0800

    Create e313.cpp

---
+++ ZJ/e313.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define io ios::sync_with_stdio(0);cin.tie(0); 
+
+
+
+int main() {
+    io ;
+
+    int n ;
+    pair<int,string> p[1005] ;
+    string str ;
+    int cnt ;
+    
+    cin>>n ;
+    for (int i = 0 ; i < n; i++){
+        cnt = 0 ;
+        cin>>str ;
+        p[i].second = str ;
+        sort(str.begin(),str.end()) ;
+        for (int j = 1; j < str.size(); j++){
+            if (str[j]!=str[j-1]) cnt++ ;    
+        }
+            p[i].first = cnt ;
+        }
+        
+    sort(p,p+n) ;
+    cout<<p[0].second ;
+
+    return 0 ;
+
+}
