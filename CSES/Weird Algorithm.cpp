f97f08431a37320f44eac5febd55de75c2b25986
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2026-03-28 01:14:00 +0800

    Create Weird Algorithm.cpp

---
+++ CSES/Weird Algorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define io ios::sync_with_stdio(0);cin.tie(0); 
+
+
+int main() {
+    io ;
+
+    ll n ;
+
+    cin>>n ;
+    while(true){
+        cout<<n<<' ' ;
+        if (n==1) break ;
+        if(n%2==0) n /=2 ;
+        else n = n*3+1 ;
+    }
+
+    return 0 ;
+
+}
