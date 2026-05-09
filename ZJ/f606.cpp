9998e399fecdfb46da9985b6aef557d556fa4575
abyssbreeze <118946392+abyssbreeze@users.noreply.github.com>
2026-05-10 03:44:51 +0800

    Update f606.cpp

--- ZJ/f606.cpp
+++ ZJ/f606.cpp
@@ -5,42 +5,50 @@ using namespace std;
 #define io ios::sync_with_stdio(0);cin.tie(0); 
 #define maxint -2147483648
 #define minint 2147483647
+#define maxll -9223372036854775808
+#define minll 9223372036854775807
 
 
 int main() {
     io ;
 
-    int n , m , k ;
-    int mi = minint ;
-    int city ;
-    int cost = 0 ;
+    ll n , m , k ;
+    ll mi = minll ;
+    ll city ;
+    ll cost ;
     cin>>n>>m>>k ;
-
-    int q[n+5][m+5] = {} ;
-    int flow[n+5][m+5] = {} ;
     
-    for (int i = 0 ; i < n ; i++){
-        for (int j = 0 ; j < n ; j++){
+    ll q[n+5][m+5] = {} ;
+    ll flow[m+5][m+5] = {} ;
+
+    
+    for (ll i = 0 ; i < n ; i++){
+        for (ll j = 0 ; j < m ; j++){
             cin>>q[i][j] ;
         }
     }
-    for (int h = 0 ; h < k ; h++)    {
-        for (int i = 0 ; i < n ; i++){
+    for (ll h = 0 ; h < k ; h++){
+        memset(flow, 0, sizeof(flow));
+        for (ll i = 0 ; i < n ; i++){
             cin>>city ;
-            for (int j = 0 ; j < m ; j++){
+            for (ll j = 0 ; j < m ; j++){
                 flow[city][j] += q[i][j] ;
             }
-        }    
-    }    
-    for(int i=0;i<m;i++){
-        for(int j=0;j<m;j++){
-            if(i==j) cost+=flow[i][j];
-            else{
-                if(flow[i][j]>=1000) cost+=3000+2*(flow[i][j]-1000);
-                else cost+=3*(flow[i][j]);
+        }
+        cost = 0 ;    
+        for(ll i=0;i<m;i++){
+            for(ll j=0;j<m;j++){
+                if(i==j) cost+=flow[i][j];
+                else{
+                    if(flow[i][j]<=1000) cost += 3*(flow[i][j]) ;
+                    else cost += 3000+2*(flow[i][j]-1000);
+                }
             }
         }
-    }
+        mi = min(cost,mi) ;
+    }    
+    
+    cout<<mi<<'\n' ;
         
     return 0 ;
 
